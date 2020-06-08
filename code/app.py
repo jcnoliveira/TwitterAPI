7c3ebef3612860f378ae3c40167d0ccbaf19c6af
Julio Cesar Oliveira <jcn.oliveira@outlook.com>
2020-06-08 02:24:56 -0300

    Adicionado projeto em django. Adicionado Stack do prometheus. Ajustes no arquivo de extração do twitter, agora com mongodb. Alterações no docker-compose

---
+++ code/app.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+from flask import Flask, jsonify, request
+from elasticsearch import Elasticsearch
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from prometheus_client import make_wsgi_app
+
+
+es = Elasticsearch([{'host': 'localhost'}])
+
+app = Flask(__name__)
+
+# Add prometheus wsgi middleware to route /metrics requests
+app_dispatch = DispatcherMiddleware(app, {
+    '/metrics': make_wsgi_app()
+})
+
+@app.route('/', methods=['GET'])
+def index():
+    results = es.get(index='contents', doc_type='title', id='my-new-slug')
+    return jsonify(results['_source'])
+
+
+@app.route('/insert_data', methods=['POST'])
+def insert_data():
+    slug = request.form['slug']
+    title = request.form['title']
+    content = request.form['content']
+
+    body = {
+        'slug': slug,
+        'title': title,
+        'content': content,
+        'timestamp': datetime.now()
+    }
+
+    result = es.index(index='contents', doc_type='title', id=slug, body=body)
+
+    return jsonify(result)
+
+@app.route('/search', methods=['POST'])
+def search():
+    keyword = request.form['keyword']
+
+    body = {
+        "query": {
+            "multi_match": {
+                "query": keyword,
+                "fields": ["content", "title"]
+            }
+        }
+    }
+
+    res = es.search(index="contents", doc_type="title", body=body)
+
+    return jsonify(res['hits']['hits'])
+
+#app.run(port=8090, debug=True)
--- code/twitter.py
+++ code/twitter.py
@@ -1,131 +1,74 @@
 # acessar https://apps.twitter.com para criar uma nova aplicação
 # cada aplicação tem suas próprias chaves
 from cmreslogging.handlers import CMRESHandler
+from pymongo import MongoClient
+from datetime import datetime
 import pandas as pd
 import logging
 import tweepy
+import json
 import sys
 import re
 import os
 
+def autentica_twitter():
+    auth = tweepy.OAuthHandler('PnDbc1eIqAnsDqS8yNWIKMRls', 
+                            'CEJuw59mJ3hZOgoARS2Z8GT6OhBnnxlylkNzDKx8rXMpjXWu2e')
+    auth.set_access_token('1243273842845454338-OQhmg52U32DTMbtaj6sb1mc3Yu4uHm',
+                        'KOwORproGuGFrREs5jlIebq4jzZsRRu7VrJ7ZgQO0smpD')
+    return tweepy.API(auth)
 
 
-# acessar a aba "Keys and Access Tokens"
-# passa o Consumer Key e o Consumer Secret
-auth = tweepy.OAuthHandler('PnDbc1eIqAnsDqS8yNWIKMRls', 'CEJuw59mJ3hZOgoARS2Z8GT6OhBnnxlylkNzDKx8rXMpjXWu2e')
-
-# define o token de acesso
-# para criar basta clicar em "Create my access token"
-# passa o "Access Token" e o "Access Token Secret"
-auth.set_access_token('1243273842845454338-OQhmg52U32DTMbtaj6sb1mc3Yu4uHm',
-		'KOwORproGuGFrREs5jlIebq4jzZsRRu7VrJ7ZgQO0smpD')
-
-# cria um objeto api
-api = tweepy.API(auth)
-
-# obtém tweets de um dado usuário
-def obter_tweets_usuario(usuario, limite=50):
-	resultados = api.user_timeline(screen_name=usuario, count=limite, tweet_mode='extended')
-	tweets = [] # lista de tweets inicialmente vazia
-	for r in resultados:
-		# utiliza expressão regular para remover a URL do tweet
-		# http pega o início da url
-		# \S+ pega os caracteres não brancos (o final da URL) 
-		tweet = re.sub(r'http\S+', '', r.full_text)
-		tweets.append(tweet.replace('\n', ' ')) # adiciona na lista
-	return tweets # retorna a lista de tweets
-
-def procura_hashtag(hashtag, quantidade_procura=1 ):
-    df = pd.DataFrame(columns=['tweet_id', 
-                            'hashtag', 
-                            'date', 
-                            'text', 
-                            'username', 
-                            'name', 
-                            'user_id', 
-                            'followers_count', 
-                            'location', 
-                            'source', 
-                            'source_url', 
-                            'lang'])
-    msgs = []
-    msg =[]
-    search_string = hashtag + ' -filter:retweets'
+def procura_hashtag(api, hashtag, mongo, quantidade_procura=100 ):
     for tweet in tweepy.Cursor(api.search, 
-                            q=str(search_string), 
+                            q=str(hashtag + ' -filter:retweets'), 
                             rpp=100, 
                             tweet_mode='extended', 
                             result_type='recent').items(quantidade_procura):
-        print(tweet.full_text)
-        print(tweet.lang)
-        print("===============================[")
-        msg = [tweet.id, 
-                hashtag, 
-                tweet.created_at, 
-                tweet.full_text, 
-                tweet.user.screen_name, 
-                tweet.user.name, 
-                tweet.user.id, 
-                tweet.user.followers_count, 
-                tweet.user.location, 
-                tweet.source, 
-                tweet.source_url, 
-                tweet.lang] 
-        msg = tuple(msg)                    
-        msgs.append(msg)
-
-
+        msg = {"id"              : tweet.id, 
+               "hashtag"         : hashtag, 
+               "created_at"      : tweet.created_at, 
+               "full_text"       : tweet.full_text, 
+               "username"        : tweet.user.screen_name, 
+               "name"            : tweet.user.name, 
+               "user_id"         : tweet.user.id, 
+               "followers_count" : tweet.user.followers_count, 
+               "location"        : tweet.user.location, 
+               "source"          : tweet.source, 
+               "source_url"      : tweet.source_url, 
+               "lang"            : tweet.lang}
         
-    df = pd.DataFrame(list(msgs))
-    print('------------------------------------------')
-    print(msgs)
-    print(df)
-    return df
+        grava_dados(msg, mongo)
+    return msg
 
 
-# escreve os tweets em um arquivo 'tweets.txt'
-#tweets = obter_tweets_usuario(usuario='jairbolsonaro', limite=100)
-#with open('tweets.txt', 'w') as f:
-#	f.write('\n'.join(tweets))
+def cria_conexao_mongo(host='localhost', port=27017, username='root', password='example'): 
+    cliente = MongoClient(host, port, username, password)
+    banco = cliente['twitter']
+    return banco['tweets']
 
 
-hashtags = ["#openbanking", 
-            "#remediation", 
-            "#devops", 
-            "#sre", 
-            "#microservices", 
-            "#observability", 
-            "#oauth", 
-            "#metrics", 
-            "#logmonitoring", 
-            "#opentracing"]
-for x in hashtags:
-    data = pd.DataFrame(columns=['tweet_id', 
-                            'hashtag', 
-                            'date', 
-                            'text', 
-                            'username', 
-                            'name', 
-                            'user_id', 
-                            'followers_count', 
-                            'location', 
-                            'source', 
-                            'source_url', 
-                            'lang'])   
-    data.append(procura_hashtag(x))
+def grava_dados(data, mongo):
+    result = mongo.insert_one(data).inserted_id
+    print("linha inserida: " + str(result))
 
-data.columns = ['tweet_id', 
-                'hashtag', 
-                'date', 
-                'text', 
-                'username', 
-                'name', 
-                'user_id', 
-                'followers_count', 
-                'location', 
-                'source', 
-                'source_url', 
-                'lang']
-print(data)
-print(data.dtypes)
-print(data.columns.values) 
+
+def busca_hashtag(hashtag):
+    for x in hashtags:
+       procura_hashtag(x, cria_conexao_mongo(), autentica_twitter())
+
+
+
+
+if __name__ == "__main__":
+    hashtags = ["#openbanking", 
+                "#remediation",
+                "#devops", 
+                "#sre", 
+                "#microservices"]#,
+                #"#observability", 
+                #"#oauth", 
+                #"#metrics", 
+                #"#logmonitoring", 
+                #"#opentracing"]
+    busca_hashtag(hashtags)
